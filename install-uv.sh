bbbb3d9fb5aa8425e3259005cabe4b97d7ef8d88
huseinzolkepliscicom <husein.zolkepli@scicom.com.my>
2025-10-19 00:14:19 +0800

    fix install

---
+++ install-uv.sh
@@ -0,0 +1,17 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+uv venv --python 3.12 --allow-existing
+source .venv/bin/activate
+uv pip install torch==2.8.0
+uv pip install transformers
+uv pip install accelerate
+uv pip install ipython ipykernel
+uv pip install mosaicml-streaming
+uv pip install datasets
+uv pip install wandb
+uv pip install pandas pyarrow
+uv pip install multiprocess
+uv pip install --no-build-isolation transformer_engine[pytorch]
+FLASH_ATTENTION_SKIP_CUDA_BUILD="TRUE" uv pip install flash-attn==2.8.3 --no-build-isolation
+uv pip install git+https://github.com/apple/ml-cross-entropy
+ipython kernel install --user --name=small-malaysian-lm
--- install.sh
+++ install.sh
@@ -1,17 +1,12 @@
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-uv venv --python 3.12 --allow-existing
-source .venv/bin/activate
-uv pip install torch==2.8.0
-uv pip install transformers
-uv pip install accelerate
-uv pip install ipython ipykernel
-uv pip install mosaicml-streaming
-uv pip install datasets
-uv pip install wandb
-uv pip install pandas pyarrow
-uv pip install multiprocess
-uv pip install --no-build-isolation transformer_engine[pytorch]
-FLASH_ATTENTION_SKIP_CUDA_BUILD="TRUE" uv pip install flash-attn==2.8.3 --no-build-isolation
-uv pip install git+https://github.com/apple/ml-cross-entropy
-ipython kernel install --user --name=small-malaysian-lm
+
+pip3 install torch==2.8.0
+pip3 install transformers==4.57.1
+pip3 install accelerate==1.10.1
+pip3 install mosaicml-streaming
+pip3 install datasets
+pip3 install wandb
+pip3 install pandas pyarrow
+pip3 install multiprocess
+pip3 install --no-build-isolation transformer_engine[pytorch]
+FLASH_ATTENTION_SKIP_CUDA_BUILD="TRUE" pip3 install flash-attn==2.8.3 --no-build-isolation
+pip3 install git+https://github.com/apple/ml-cross-entropy
